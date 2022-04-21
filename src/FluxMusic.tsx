5f5b5fbadde4c53e9b10c6bdbc3abaff98a8cfe2
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-21 13:32:24 -0300

    [feat]: importing compoent news in index

---
+++ src/FluxMusic.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Index from './components';
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Index />
+    </div>
+  )
+}
+
+export default App
