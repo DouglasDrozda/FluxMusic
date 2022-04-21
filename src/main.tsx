76fb59ecdf47242b27360ea779c55dafa03ab87e
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-21 12:36:30 -0300

    [chore]: config index with Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import FluxMusic from './FluxMusic'
+import { Provider } from './context/Provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <FluxMusic />
+    </Provider>
   </React.StrictMode>
 )
