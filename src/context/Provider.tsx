a2be9f12e815be44363a77f8a89cedc1dba5ed7d
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 17:52:18 -0300

    [feat]: creating function to render the disks on the screen and filter logic

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Context from './index';
-import { resquestNews, resquestArtists } from '../services/apiRequest';
-import { INews, IArtist } from '../interfaces';
+import { resquestNews, resquestArtists, resquestDiscs } from '../services/apiRequest';
+import { INews, IArtist, IDiscs } from '../interfaces';
 import infoFromNews from '../dataImages';
 import imageArtists from '../dataImagesArtists';
+import infosDiscs from '../dataInfosDiscs';
 
 interface Iprops {
   children: JSX.Element | JSX.Element[];
@@ -13,10 +14,13 @@ export function Provider({ children }: Iprops): JSX.Element {
   const [isLoading, setIsLoading] = useState(true);
   const [dataNews, setDataNews] = useState<INews[] | undefined>();
   const [dataArtists, setDataArtists] = useState<IArtist[] | undefined>();
+  const [dataDiscs, setDataDiscs] = useState<IDiscs[] | undefined>();
   const [searchInput, setSearchInput] = useState<string>('');
   const [filterNews, setFilterNews] = useState<INews[] | undefined>(dataNews);
   const [searchInputArtist, setSearchInputArtist] = useState<string>('');
   const [filterArtist, setFilterArtist] = useState<IArtist[] | undefined>(dataArtists);
+  const [searchInputDiscs, setSearchInputDiscs] = useState<string>('');
+  const [filterDiscs, setFilterDiscs] = useState<IDiscs[] | undefined>(dataDiscs);
 
   const getNews = async (): Promise<void> => {
     const response = await resquestNews() as INews[] | undefined;
@@ -61,29 +65,59 @@ export function Provider({ children }: Iprops): JSX.Element {
       setFilterArtist(includeArtist);
   }
 
+  const getDiscs = async (): Promise<void> => {
+    const response = await resquestDiscs() as IDiscs[] | undefined;
+    const newResponse = response?.map((disc) => {
+      const infos = infosDiscs.find((info) => info.id === disc.id);
+
+      return {
+        ...disc, ...infos
+      }
+    });
+    
+    setDataDiscs(newResponse);
+    setIsLoading(false);
+  };
+
+  const getFilterDiscs = async (): Promise<void> => {
+    const includeDiscs = dataDiscs
+    ?.filter((item) =>
+      item.title.toLowerCase().includes(searchInputDiscs.toLowerCase()));
+      setFilterDiscs(includeDiscs);
+  }
+
   useEffect(() => {
     getNews();
     getArtists();
-  }, [])
+    getDiscs();
+  }, []);
 
   useEffect(() => {
     getFilterNews();
-  }, [searchInput])
+  }, [searchInput]);
 
   useEffect(() => {
     getFilterArtists();
-  }, [searchInputArtist])
+  }, [searchInputArtist]);
+
+  useEffect(() => {
+    getFilterDiscs();
+  }, [searchInputDiscs])
 
   const context = {
     isLoading,
     dataNews,
     dataArtists,
+    dataDiscs,
     searchInput,
     setSearchInput,
     searchInputArtist,
     setSearchInputArtist,
     filterNews,
     filterArtist,
+    searchInputDiscs,
+    setSearchInputDiscs,
+    filterDiscs,
   };
 
   return <Context.Provider value={{ context }}>{children}</Context.Provider>;
