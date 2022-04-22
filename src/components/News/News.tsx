7c3ef8ddcf2d2a72c4dd86f43c4f4d311c66a456
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 19:50:56 -0300

    [refactor]: adding semantics tags

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -9,25 +9,24 @@ function News(): JSX.Element {
   return (
     <section className="news-container">
       {isLoading ? <p>loading....</p> :
-        <div className="news-sub-container">
+        <ul className="news-sub-container">
           <h1>Notícias</h1>
           {(filterNews === undefined ? dataNews : filterNews)?.map((data) => (
-            <div className="news-content-contain" id="news" key={data.id}>
+            <li className="news-content-contain" id="news" key={data.id}>
               <div className="news-content">
-                <a href={data.url} target="_blank">{data.title}</a>
                 <div className="img-news">
                   <img src={data.img} alt={data.alt} />
+                  <div className="news-infos">
+                    <a href={data.url} target="_blank">{data.title}</a>
+                    <p>{data.subtitle}</p>
+                    <p>{`Escritor: ${data.writer}`}</p>
+                    <a href={data.url} target="_blank" id="view-more">Ver mais...</a>
+                  </div>
                 </div>
-                <p>{data.subtitle}</p>
-                {/* <div dangerouslySetInnerHTML={{__html: data.content}}>
-                  
-                </div> */}
-                <p>{`Escritor: ${data.writer}`}</p>
-                <a href={data.url} target="_blank" id="view-more">Ver mais...</a>
               </div>
-            </div>
+            </li>
           ))}
-        </div>
+        </ul>
       }
     </section>
   );
