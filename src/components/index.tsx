09f3634d7eeb2b6bb4cdf16427837cf6c5ac2c3d
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 12:26:04 -0300

    [feat]: importing component InputSearch in index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
+import InputSearch from './InputSearch/InputSearch';
 import News from './News/News';
 
 function Index(): JSX.Element {
   return (
     <main>
       <Header />
+      <InputSearch />
       <News />
     </main>
   );
