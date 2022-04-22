1bf2b8d154836bf71ea17a27f0467970d7861f55
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 12:25:29 -0300

    [feat]: creating filter news logic and adding images news

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -4,19 +4,26 @@ import '../../styles/News.css';
 // import dataImages from '../../dataImages';
 
 function News(): JSX.Element {
-  const { isLoading, dataNews } = useContext(context).context;
+  const { isLoading, dataNews, filterNews } = useContext(context).context;
 
   return (
     <section className="news-container">
       {isLoading ? <p>loading....</p> :
         <div className="news-sub-container">
           <h1>Notícias</h1>
-          {dataNews?.map((data) => (
-            <div className="news-content-contain" key={data.id}>
+          {(filterNews === undefined ? dataNews : filterNews)?.map((data) => (
+            <div className="news-content-contain" id="news" key={data.id}>
               <div className="news-content">
                 <a href={data.url} target="_blank">{data.title}</a>
+                <div className="img-news">
+                  <img src={data.img} alt={data.alt} />
+                </div>
                 <p>{data.subtitle}</p>
+                {/* <div dangerouslySetInnerHTML={{__html: data.content}}>
+                  
+                </div> */}
                 <p>{`Escritor: ${data.writer}`}</p>
+                <a href={data.url} target="_blank" id="view-more">Ver mais...</a>
               </div>
             </div>
           ))}
