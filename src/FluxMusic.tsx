441fe028c1982591d1f4ad30717977884c32cf6d
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 15:55:57 -0300

    [refactor]: adding pages routes

--- src/FluxMusic.tsx
+++ src/FluxMusic.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Index from './components';
+import Artists from './components/Pages/Artists';
 
 function App(): JSX.Element {
   return (
     <div>
-      <Index />
+      <Routes>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/artists" element={<Artists />}></Route>
+      </Routes>
     </div>
   )
 }
