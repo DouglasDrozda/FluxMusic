c48656882c803690655ca44bcdae370807b74ce0
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 12:17:49 -0300

    [feat]: creating news images object

--- src/dataImages.tsx
+++ src/dataImages.tsx
@@ -1,15 +1,43 @@
 const img = [
   {
-    img: 'https://i.f1g.fr/media/figarofr/616x347_cropupscale/2014/03/04/PHOe1a1f484-9e34-11e3-aba9-8275e495bb35-805x453.jpg',
-  },
-  {
-    img: 'https://i.f1g.fr/media/figarofr/616x347_cropupscale/2014/03/04/PHOe1a1f484-9e34-11e3-aba9-8275e495bb35-805x453.jpg',
-  },
-  {
+    id: 11243,
+    alt: 'img-test',
     img: 'https://www.liberation.fr/resizer/IicG6I6Er3WyvF0J9-Ue8EKHXUs=/800x0/filters:format(png):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/32U7T2FL4VP75NUCULDRGGQ4IU.png',
   },
   {
+    id: 11550,
+    alt: 'img-test',
+    img: 'https://i.f1g.fr/media/figarofr/616x347_cropupscale/2014/03/04/PHOe1a1f484-9e34-11e3-aba9-8275e495bb35-805x453.jpg',
+  },
+  {
+    id: 11885,
+    alt: 'img-test',
     img: 'https://www.liberation.fr/resizer/Asbq8JZTEuJiCCVuqWgxvkC3XcA=/800x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/RZRBFE3UZSUQ2VLWQU3G2BU5W4.jpg',
+  },
+  {
+    id: 11891,
+    alt: 'img-test',
+    img: 'https://www.liberation.fr/resizer/-2xQOy-7qfUu81--qktYjFxgNWA=/800x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/AQHD3NQDB6QFD7V5S3YINMTNIQ.jpg',
+  },
+  {
+    id: 12055,
+    alt: 'img-test',
+    img: 'https://i.f1g.fr/media/figarofr/704x396_cropupscale/2014/03/21/PHO8c374f48-b023-11e3-93c9-a08eb5efd229-805x453.jpg',
+  },
+  {
+    id: 12187,
+    alt: 'img-test',
+    img: 'https://www.liberation.fr/resizer/dv0p4bUODYRyx8e0xG6CXEinj0g=/800x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/YSPNZ5245UTV4WH4RVWGK6KO7Y.jpg',
+  },
+  {
+    id: 12269,
+    alt: 'img-test',
+    img: 'https://i.f1g.fr/media/figarofr/704x396_cropupscale/2014/05/12/PHObf11377a-d5ef-11e3-bead-b7c383cc0246-805x453.jpg',
+  },
+  {
+    id: 24866,
+    alt: 'img-test',
+    img: 'https://www.lesinrocks.com/wp-content/uploads/2013/12/lateef.jpg?resize=604%2C465&quality=75',
   }
 ]
 
