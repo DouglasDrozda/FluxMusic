579c4926e83e49a535d381b937b1e0dc49b9b0db
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 15:55:07 -0300

    [refactor]: adding BrowserRouter in index aplication

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FluxMusic from './FluxMusic'
 import { Provider } from './context/Provider';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider>
-      <FluxMusic />
-    </Provider>
+    <BrowserRouter>
+      <Provider>
+        <FluxMusic />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 )
