8536d258d6a2586f401973dc66b7f3e1139a9431
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 19:49:48 -0300

    [refactor]: adding news images

--- src/dataImages.tsx
+++ src/dataImages.tsx
@@ -1,42 +1,52 @@
 const img = [
   {
+    id: 10372,
+    alt: 'img-Programmation Jazz',
+    img: 'https://frenchculture.org/sites/default/files/styles/huge/public/field/image/jazz-a-la-villette_2020.jpg?itok=7hzULTzl',
+  },
+  {
     id: 11243,
-    alt: 'img-test',
+    alt: 'img-François Corneloup',
     img: 'https://www.liberation.fr/resizer/IicG6I6Er3WyvF0J9-Ue8EKHXUs=/800x0/filters:format(png):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/32U7T2FL4VP75NUCULDRGGQ4IU.png',
   },
   {
     id: 11550,
-    alt: 'img-test',
+    alt: 'img-Gregory Porter,',
     img: 'https://i.f1g.fr/media/figarofr/616x347_cropupscale/2014/03/04/PHOe1a1f484-9e34-11e3-aba9-8275e495bb35-805x453.jpg',
   },
   {
     id: 11885,
-    alt: 'img-test',
+    alt: 'img-Archie Shepp',
     img: 'https://www.liberation.fr/resizer/Asbq8JZTEuJiCCVuqWgxvkC3XcA=/800x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/RZRBFE3UZSUQ2VLWQU3G2BU5W4.jpg',
   },
   {
     id: 11891,
-    alt: 'img-test',
+    alt: 'img-Lesprit de Mingus',
     img: 'https://www.liberation.fr/resizer/-2xQOy-7qfUu81--qktYjFxgNWA=/800x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/AQHD3NQDB6QFD7V5S3YINMTNIQ.jpg',
   },
   {
     id: 12055,
-    alt: 'img-test',
+    alt: 'img-Parisien',
     img: 'https://i.f1g.fr/media/figarofr/704x396_cropupscale/2014/03/21/PHO8c374f48-b023-11e3-93c9-a08eb5efd229-805x453.jpg',
   },
   {
     id: 12187,
-    alt: 'img-test',
+    alt: 'img-Le jazz baroque',
     img: 'https://www.liberation.fr/resizer/dv0p4bUODYRyx8e0xG6CXEinj0g=/800x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/YSPNZ5245UTV4WH4RVWGK6KO7Y.jpg',
   },
   {
     id: 12269,
-    alt: 'img-test',
+    alt: 'img-Onirique',
     img: 'https://i.f1g.fr/media/figarofr/704x396_cropupscale/2014/05/12/PHObf11377a-d5ef-11e3-bead-b7c383cc0246-805x453.jpg',
   },
   {
+    id: 24217,
+    alt: 'img-Yusef Lateef',
+    img: 'https://www.paris-move.com/wp-content/uploads/2018/03/Marjolaine-Reymond.jpg',
+  },
+  {
     id: 24866,
-    alt: 'img-test',
+    alt: 'img-souffle',
     img: 'https://www.lesinrocks.com/wp-content/uploads/2013/12/lateef.jpg?resize=604%2C465&quality=75',
   }
 ]
