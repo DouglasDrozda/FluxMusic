68b7b47964af1b8530e7cc639e2a2af07a63e166
DouglasDrozda <lowdouglas9712@gmail.com>
2022-04-22 23:30:48 -0300

    [chore]: testing deploy

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,15 +1,15 @@
 import axios from 'axios';  
 
 const apiNews = axios.create({
-  baseURL: import.meta.env.VITE_TIMELINE,
+  baseURL: import.meta.env.VITE_TIMELINE || 'https://api.music-story.com/en/genre/21/',
 });
 
 const apiArtists = axios.create({
-  baseURL: import.meta.env.VITE_ARTISTS,
+  baseURL: import.meta.env.VITE_ARTISTS || 'http://api.music-story.com/en/genre/1/',
 });
 
 const apiDiscs = axios.create({
-  baseURL: import.meta.env.VITE_DISCS,
+  baseURL: import.meta.env.VITE_DISCS || 'http://api.music-story.com/en/genre/1/',
 });
 
 
